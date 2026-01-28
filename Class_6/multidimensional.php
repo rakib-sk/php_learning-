a6d4c1a7216cca92e2a5e03db413fde84e4e1134
Rakib Hoseen <rh01828315879@gmail.com>
2026-01-28 09:19:27 +0600

    add multidimensional array

---
+++ Class_6/multidimensional.php
@@ -0,0 +1,29 @@
+<?php
+$student = [
+    [
+        "name" => "Sanjida Aktar",
+        "age" => 21,  
+        "depertment" => "CSE",
+        $subject = [
+            "Dicrete Math",
+            "Electronics",
+            "Algorithm",
+            "Statistic",
+            ]
+    ],
+    [
+        "name" => "Rakib Hossen",
+        "age" => 18,  
+        "depertment" => "CST",
+        $subject = [
+            "Mathematics",
+            "Bangla - 1",
+            "English - 1",
+            "Physics - 1",
+            "Basic Electricity",
+            "Computer office application"
+            ]
+    ]
+];
+print_r();
+
